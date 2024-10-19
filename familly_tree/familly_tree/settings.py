fe6b7a6563132820e451710151155d8780298745
arturgorski14 <arturgorski14@gmail.com>
2024-10-19 12:13:19 +0200

    saving local work

--- familly_tree/familly_tree/settings.py
+++ familly_tree/familly_tree/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--)x9sv8(z8m8vr%8gy*+w8k6fe^c5775-!@2p4ofs8$mk!tw=l"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
-
 
 # Application definition
 
