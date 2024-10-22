dd78f9a68b244cccc52625a18fadb475fa228306
arturgorski14 <arturgorski14@gmail.com>
2024-10-22 21:35:56 +0200

    feat: AddNew as FormView

--- familly_tree/members/urls.py
+++ familly_tree/members/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("", views.main, name="main"),
     path(f"{app_name}/", views.AllMembers.as_view(), name="members"),
     path(f"{app_name}/<int:pk>", views.Details.as_view(), name="details"),
-    path(f"{app_name}/add/", views.add_new, name="add"),
+    path(f"{app_name}/add/", views.AddNew.as_view(), name="add"),
     path(f"{app_name}/edit/<int:pk>", views.edit, name="edit"),
     path(f"{app_name}/remove/<int:pk>", views.remove, name="remove"),
 ]
--- familly_tree/members/views.py
+++ familly_tree/members/views.py
@@ -1,8 +1,8 @@
 import logging
 
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, QueryDict
 from django.shortcuts import get_object_or_404, render
-from django.views import generic
+from django.views import generic, View
 
 from .forms import MemberForm
 from .models import Member
@@ -21,9 +21,38 @@ class Details(generic.DetailView):
     template_name = "details.html"
 
 
-def add_new(request):
-    if request.POST:
-        data: MemberForm = request.POST
+class AddNew(generic.FormView):
+    form_class = MemberForm
+    template_name = "add_new_member.html"
+
+    def get(self, request, *args, **kwargs):
+        context = {"form": self.form_class()}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            data = self._clean_data(request.POST)
+            new_member = Member(
+                firstname=data["firstname"],
+                lastname=data["lastname"],
+                family_lastname=data["family_lastname"],
+                sex=data["sex"],
+                birth_date=data["birth_date"],
+                is_alive=data["is_alive"],
+                death_date=data["death_date"],
+                children_num=data["children_num"],
+                father_id=data["father_id"],
+                mother_id=data["mother_id"],
+            )
+            new_member.save()
+            return HttpResponseRedirect("/members/")
+        context = {"form": form}
+        return render(request, self.template_name, context)
+
+    def _clean_data(self, data: QueryDict) -> dict:  # Something like marshmallow schema would be usefull
+        cleaned_data = data.copy()
+        cleaned_data.pop("csrfmiddlewaretoken")
         set_is_alive = data["is_alive"] == "on" if "is_alive" in data else True
         set_birthdate = (
             data["birth_date"]
@@ -35,26 +64,15 @@ def add_new(request):
             if "death_date" in data and data["death_date"]
             else None  # noqa E501
         )
-        new_member = Member(
-            firstname=data["firstname"],
-            lastname=data["lastname"],
-            family_lastname=data["family_lastname"],
-            sex=data["sex"],
-            birth_date=set_birthdate,
-            is_alive=set_is_alive,
-            death_date=set_deathdate,
-            children_num=data["children_num"],
-        )
-        new_member.save()
-        return HttpResponseRedirect("/members")
-    template = "add_new_member.html"
-    form = MemberForm()
-    context = {
-        "error_message": "You didn't select a choice.",
-        "form": form,
-    }
-    return render(request, template, context)
+        set_father_id = data["father_id"] if data["father_id"] else None
+        set_mother_id = data["mother_id"] if data["mother_id"] else None
 
+        cleaned_data["is_alive"] = set_is_alive
+        cleaned_data["birth_date"] = set_birthdate
+        cleaned_data["death_date"] = set_deathdate
+        cleaned_data["father_id"] = set_father_id
+        cleaned_data["mother_id"] = set_mother_id
+        return cleaned_data
 
 def edit(request, pk: int):
     member = get_object_or_404(Member, id=pk)
