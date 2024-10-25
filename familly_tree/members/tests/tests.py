3525987c7c42e2be0edffcc4e9bfedd79821b8d7
arturgorski14 <arturgorski14@gmail.com>
2024-10-25 18:45:34 +0200

    fix: pytest setup

--- familly_tree/members/tests/tests.py
+++ familly_tree/members/tests/tests.py
@@ -1,3 +1,4 @@
+import pytest
 from django.contrib.auth.models import User
 from django.test import TestCase  # noqa E501
 
@@ -11,6 +12,7 @@ def test_example_passing():
     assert 1 == 1, "passed"
 
 
-# def test_create_user():
-#     User.objects.create_user('test', 'test@test.com', 'pass')
-#     assert User.objects.count() == 1
+@pytest.mark.django_db
+def test_create_user():
+    User.objects.create_user('test', 'test@test.com', 'pass')
+    assert User.objects.count() == 1
