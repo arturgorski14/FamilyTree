5ff3827463727c26769627765438b8ec5ec349da
arturgorski14 <arturgorski14@gmail.com>
2024-11-01 23:13:03 +0100

    feat: Added validation and tests

--- familly_tree/members/models.py
+++ familly_tree/members/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -35,3 +36,63 @@ class Member(models.Model):
             else ""  # noqa E501
         )
         return f"{self.firstname} {self.lastname} {born} {died}"
+
+    def clean(self):
+        super().clean()
+        self._validate_sex()
+        self._validate_children_num_is_not_negative()
+        self._validate_is_alive_with_death_date()
+        self._validate_if_birthdate_is_before_death_date()
+        self._validate_father_and_mother()
+
+    def save(self, *args, **kwargs):
+        self.clean()  # Call clean to validate fields before saving
+        super().save(*args, **kwargs)
+
+    def _validate_sex(self):
+        if self.sex not in (self.Sex.MALE, self.Sex.FEMALE):
+            raise ValidationError("Diversity not supported. Sex must be 'm' or 'f'")
+
+    def _validate_children_num_is_not_negative(self):
+        if self.children_num < 0:
+            raise ValidationError("Children number cannot be negative")
+
+    def _validate_is_alive_with_death_date(self):
+        if self.is_alive and self.death_date:
+            raise ValidationError("Living members cannot have a death date")
+
+    def _validate_if_birthdate_is_before_death_date(self):
+        if not self.birth_date or not self.death_date:
+            return
+        if self.birth_date > self.death_date:
+            raise ValidationError("Birth date must be before death date")
+
+    def _validate_father_and_mother(self):
+        if not self.father_id and not self.mother_id:
+            return
+        if self.father_id and self.father_id == self.id:
+            raise ValidationError("A member cannot be their own father.")
+        if self.mother_id and self.mother_id == self.id:
+            raise ValidationError("A member cannot be their own mother.")
+
+        parent_ids = [pid for pid in (self.father_id, self.mother_id) if pid]
+        parents = Member.objects.filter(id__in=parent_ids).values("id", "sex")
+        parent_lookup = {parent["id"]: parent["sex"] for parent in parents}
+
+        if self.father_id:
+            if (
+                self.father_id not in parent_lookup
+                or parent_lookup[self.father_id] != self.Sex.MALE
+            ):
+                raise ValidationError(
+                    "Non-existent or invalid father: Father must exist and be male."
+                )
+
+        if self.mother_id:
+            if (
+                self.mother_id not in parent_lookup
+                or parent_lookup[self.mother_id] != self.Sex.FEMALE
+            ):
+                raise ValidationError(
+                    "Non-existent or invalid mother: Mother must exist and be female."
+                )
---
+++ familly_tree/members/tests/test_members.py
@@ -0,0 +1,196 @@
+import factory
+import pytest
+from django.core.exceptions import ValidationError
+from django.test import TestCase  # noqa E501
+
+from members.forms import MemberForm
+from members.models import Member
+
+
+class MemberFactory(factory.Factory):
+    class Meta:
+        model = Member
+
+    firstname = "John"
+    lastname = "Doe"
+    family_lastname = "Doe"
+    sex = "m"
+    birth_date = None
+    is_alive = True
+    death_date = None
+    children_num = 0
+    father_id = None
+    mother_id = None
+
+
+def test_create_default_member(db):
+    expected_data = {
+        "firstname": "John",
+        "lastname": "Doe",
+        "family_lastname": "Doe",
+        "sex": "m",
+        "birth_date": None,
+        "is_alive": True,
+        "death_date": None,
+        "children_num": 0,
+        "father_id": None,
+        "mother_id": None,
+    }
+
+    member: Member = MemberFactory()
+
+    for key, value in expected_data.items():
+        member_value = getattr(member, key)
+        assert (
+            member_value == value
+        ), f"Expected {key} to be {value}, but got {member_value}"
+
+
+def test_set_family_lastname_as_lastname(db):
+    """If family lastname haven't been passed, set it to member lastname"""
+
+
+def test_link_non_existent_father_id(db):
+    with pytest.raises(
+        ValidationError,
+        match="Non-existent or invalid father: Father must exist and be male.",
+    ):
+        member = MemberFactory.build(father_id=999)  # Assuming 999 does not exist
+        member.clean()
+
+
+def test_link_non_existent_mother_id(db):
+    with pytest.raises(
+        ValidationError,
+        match="Non-existent or invalid mother: Mother must exist and be female.",
+    ):
+        member = MemberFactory.build(mother_id=999)  # Assuming 999 does not exist
+        member.clean()
+
+
+def test_female_as_father(db):
+    mother: Member = MemberFactory.build(sex="f")
+    mother.save()
+    with pytest.raises(
+        ValidationError,
+        match="Non-existent or invalid father: Father must exist and be male.",
+    ):
+        member = MemberFactory.build(father_id=mother.id)
+        member.clean()
+
+
+def test_male_as_mother(db):
+    father: Member = MemberFactory.build(sex="m")
+    father.save()
+    with pytest.raises(
+        ValidationError,
+        match="Non-existent or invalid mother: Mother must exist and be female.",
+    ):
+        member = MemberFactory.build(mother_id=father.id)
+        member.clean()
+
+
+def test_negative_children_num(db):
+    with pytest.raises(ValidationError, match="Children number cannot be negative"):
+        member = MemberFactory.build(children_num=-1)
+        member.clean()
+
+
+def test_invalid_sex(db):
+    with pytest.raises(
+        ValidationError, match="Diversity not supported. Sex must be 'm' or 'f'"
+    ):
+        member = MemberFactory.build(sex="x")
+        member.clean()
+
+
+def test_birth_after_death(db):
+    with pytest.raises(ValidationError, match="Birth date must be before death date"):
+        member = MemberFactory.build(
+            birth_date="2000-01-01", is_alive=False, death_date="1999-01-01"
+        )
+        member.clean()
+
+
+def test_is_alive_with_death_date(db):
+    with pytest.raises(
+        ValidationError, match="Living members cannot have a death date"
+    ):
+        member = MemberFactory.build(is_alive=True, death_date="2000-01-01")
+        member.clean()
+
+
+def test_member_cannot_be_own_father(db):
+    member = MemberFactory()
+    member.save()
+    data = {
+        "firstname": member.firstname,
+        "lastname": member.lastname,
+        "family_lastname": member.family_lastname,
+        "father_id": member.id,
+        "mother_id": member.mother_id,
+        "sex": member.sex,
+        "birth_date": member.birth_date,
+        "is_alive": member.is_alive,
+        "children_num": member.children_num,
+    }
+
+    form = MemberForm(data, instance=member)
+
+    assert not form.is_valid()
+    assert "A member cannot be their own father." in form.errors["__all__"][0]
+
+
+def test_member_cannot_be_own_mother(db):
+    member = MemberFactory()
+    member.save()
+    data = {
+        "firstname": member.firstname,
+        "lastname": member.lastname,
+        "family_lastname": member.family_lastname,
+        "father_id": member.father_id,
+        "mother_id": member.id,
+        "sex": member.sex,
+        "birth_date": member.birth_date,
+        "is_alive": member.is_alive,
+        "children_num": member.children_num,
+    }
+
+    form = MemberForm(data, instance=member)
+
+    assert not form.is_valid()
+    assert "A member cannot be their own mother" in form.errors["__all__"][0]
+
+
+"""
+Test cases TODO:
+ultimately shouldn't be possible:
+- link father_id/mother_id that doesn't exist
+- link female member as father_id and vice versa
+- negative children_num
+- different sex than m/f
+- birth_date after death_date
+- is_alive=True and has death_date
+- set itself as father_id or mother_id
+
+ultimately should be possible:
+- birth_date/death_date as not full date ie yyyy-mm-dd is a full date, so yyyy-mm and yyyy should also be valid
+"""
+
+"""
+Features TODO:
+- linking children
+- linking valid father_id/mother_id, should append children for chosen id
+- linking a child should result in adding father/mother to chosen child
+
+2.0
+- improved front-end (not only list based, but view tree based)
+- CRUD and linking by performing UI operations, not solely based on buttons.
+
+Future
+- authorization.
+- view other user family.
+- add user to family and merge families.
+- possibility to add one photo for each familly member.
+- consider read-model in elasticsearch.
+"""
